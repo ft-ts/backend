337db4255134a3aabfa1e982efc418bb783e8bbb
김서우 <4nyth4me@gmail.com>
2023-08-03 20:51:02 +0900

    wip : create dto after game

--- src/pong/dto/create-pong.dto.ts
+++ src/pong/dto/create-pong.dto.ts
@@ -1,1 +1,36 @@
-export class CreatePongDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsEnum,
+  IsInt,
+} from 'class-validator';
+import { MatchType } from '../enum/matchType.enum';
+
+export class CreatePongDto {
+  @IsNotEmpty()
+  @IsInt()
+  user1_id: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  user2_id: number;
+
+  @IsNotEmpty()
+  @IsEnum(MatchType)
+  match_type: MatchType;
+
+  @IsNotEmpty()
+  @IsInt()
+  user1_score: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  user2_score: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  winner_id: number;
+
+  @IsNotEmpty()
+  timestamp: Date;
+}
