de4a94fb9dd281b2a8904f0f59b220e400044a38
김서우 <4nyth4me@gmail.com>
2023-08-03 20:49:29 +0900

    feat : change uid type

--- src/pong/dto/pong.dto.ts
+++ src/pong/dto/pong.dto.ts
@@ -1,17 +1,18 @@
 import { Socket } from 'socket.io';
 import { Ball } from '../game/entities/ball.entity';
 import { Paddle } from '../game/entities/paddle.entity';
+import { MatchType } from '../enum/matchType.enum';
 
 export interface MatchInfo {
   matchID: string;
-  matchType: string;
+  matchType: MatchType;
   user1: Socket;
   user2: Socket;
   user1_score: number;
   user2_score: number;
   user1_elo: number;
   user2_elo: number;
-  winner_id: string;
+  winner_id: number;
   start_date: Date;
   interval: NodeJS.Timeout;
 }
@@ -20,4 +21,16 @@ export interface GameInfo {
   player1: Paddle;
   player2: Paddle;
   ball: Ball;
+}
+
+export interface updateDto {
+  user1_id: number;
+  user2_id: number;
+  match_type: string;
+  user1_score: number;
+  user2_score: number;
+  user1_elo: number;
+  user2_elo: number;
+  winner_id: number;
+  timestamp: Date;
 }
