4f4c0cf6122262b2e05deb7557e1af87b9396a13
jeongmino <jeongmino1207@gmail.com>
2023-09-01 18:06:35 +0900

    feat: tokens are well communicated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+    // origin: '*',
   });
 
   const port = process.env.SERVER_PORT || 10000;
