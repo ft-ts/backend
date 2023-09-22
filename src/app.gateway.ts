a6bed8eb170e7c75d96a79ca1975bf2f32e1d21f
김서우 <4nyth4me@gmail.com>
2023-09-23 06:11:14 +0900

    feat: emit updated Info to users

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,17 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  ConnectedSocket,
+} from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { AuthService } from 'src/auth/auth.service';
 import { Logger, UseGuards } from '@nestjs/common';
 import { AtGuard } from 'src/auth/auth.guard';
 import { SocketService } from 'src/common/service/socket.service';
+import { UserStatus } from './user/enums/userStatus.enum';
 
 @UseGuards(AtGuard)
 @WebSocketGateway({
@@ -11,7 +19,8 @@ import { SocketService } from 'src/common/service/socket.service';
     origin: true,
   },
 })
-export class AppGateway {
+export class AppGateway 
+implements OnGatewayConnection, OnGatewayDisconnect{
   constructor(
     private readonly authService: AuthService,
     private readonly socketService: SocketService,
@@ -28,12 +37,36 @@ export class AppGateway {
     console.log('😭 app/handleConnection', client.data);
     await this.socketService.addSocket(client.data.uid, client);
     await client.join(`dm-${client.data.uid}`);
+    this.server.emit('update/userConnection', {uid : client.data.uid, status: UserStatus.ONLINE});
     Logger.debug(`[AppGateway] ${client.data.uid} joined 'dm-${client.data.uid}'`);
   }
 
   async handleDisconnect(client: Socket) {
     Logger.debug(`[AppGateway] ${client.data.uid} disconnected`);
     await this.socketService.removeSocket(client.data.uid);
-    this.authService.handleUserStatus(client.data.uid, false);
+    await this.authService.handleUserStatus(client.data.uid, false);
+    this.server.emit('update/userConnection', {uid: client.data.uid, status: UserStatus.OFFLINE});
+  }
+  
+  @SubscribeMessage('update/userInfo')
+  async updateMyInfo(
+    @ConnectedSocket() client: Socket,
+  ) {
+    Logger.debug(`[AppGateway] test123 ${client.data.uid}`);
+    this.server.emit('update/userInfo');
+  }
+
+  @SubscribeMessage('update/channelInfo')
+  async updateChannelInfo(
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.server.emit('update/channelInfo');
+  }
+
+  @SubscribeMessage('update/friends')
+  async updateFriends(
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.emit('update/friends');
   }
 }
